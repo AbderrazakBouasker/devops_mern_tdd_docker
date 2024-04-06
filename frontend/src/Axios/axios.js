9be129284c5cc2a3d896d1f6df3505b61aef33ae
abderrazak <abderrazakbouasker@gmail.com>
2024-04-06 15:18:17 +0000

    fixed axios prob changed port configuration

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,6 @@
-const axios = require("axios");
-const instance = axios.create({
+//const axios = require("axios");
+import axios from 'axios';
+const instance = axios.default.create({
     baseURL:"http://localhost:8000/api"
 })
-module.exports = instance;
+export default instance;
